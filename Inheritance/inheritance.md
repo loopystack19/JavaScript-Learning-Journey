223d30048b290d06a98eefa6a48b70d489079d6c
loopystack19 <sanjaygreg19@gmail.com>
2025-09-02 23:46:13 +0300

    the super keyword is super

--- Inheritance/inheritance.md
+++ Inheritance/inheritance.md
@@ -1,3 +1,54 @@
 -Inheritance is essential in code reuseability, it allows children classes to inherit properties and methods from parent classes
 
 **Sample programme**
+
+class Animal{
+
+    alive=true;
+
+    eat(){
+
+        console.log(`This ${this.name} is eating`);
+    }
+
+    sleep(){
+
+        console.log(`This ${this.name} is sleeping`);
+    }
+}
+
+class Rabbit extends Animal{
+
+    name="rabbit";
+}
+
+class Fish extends Animal{
+
+    name="fish";
+}
+
+class Hawk extends Animal{
+
+    name="hawk";
+}
+
+
+const rabbit = new Rabbit();
+
+const fish = new Fish();
+
+const hawk = new Hawk();
+
+
+console.log(rabbit.alive);
+
+rabbit.eat();
+
+rabbit.sleep();
+
+console.log(fish.alive);
+
+fish.eat();
+
+fish.sleep();
+
---
+++ super keyword/super.js
@@ -0,0 +1,92 @@
+class Animal{
+
+    constructor(name, age){
+
+        this.name=name;
+
+        this.age=age;
+
+    }
+
+    move(speed){
+
+        console.log(`This ${this.name} moves at a speed of ${speed}mph`);
+    }
+}
+
+class Rabbit extends Animal{
+
+    constructor(name, age, runSpeed){
+
+        super(name, age);
+
+         this.runSpeed = runSpeed;
+
+    }
+
+    run(){
+
+        console.log(`This ${this.name} can run`);
+
+        super.move(this.runSpeed);
+
+    }
+}
+
+class Fish extends Animal{
+
+    constructor(name, age, swimSpeed){
+
+        super(name, age);
+
+        this.swimSpeed = swimSpeed;
+
+    }
+    swim(){
+
+        console.log(`This ${this.name} can swim`);
+
+        super.move(this.swimSpeed);
+    }
+}
+
+class Hawk extends Animal{
+
+    constructor(name, age, flySpeed){
+
+        super(name, age);
+
+        this.flySpeed=flySpeed;
+    }
+
+    fly(){
+
+        console.log(`This ${this.name} can fly`);
+
+        super.move(this.flySpeed);
+    }
+}
+
+const rabbit= new Rabbit("buggsBunny", 45, 12);
+
+console.log(rabbit["name"]);
+
+console.log(rabbit["age"]);
+
+rabbit.run();
+
+const fish = new Fish("garry", 3, 23);
+
+console.log(fish["name"]);
+
+console.log(fish["age"]);
+
+fish.swim();
+
+const hawk= new Hawk("baldEagle", 65, 123);
+
+console.log(hawk["name"]);
+
+console.log(hawk["age"]);
+
+hawk.fly();
