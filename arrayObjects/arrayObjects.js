78f02c2bc1ed27cb8628c98d789f25f2d801ca4f
loopystack19 <sanjaygreg19@gmail.com>
2025-09-03 09:52:27 +0300

    array of objects are also cool

---
+++ arrayObjects/arrayObjects.js
@@ -0,0 +1,13 @@
+let fruits = [
+  { name: "Apple", color: "red", calories: 95 },
+  { name: "Banana", color: "Yellow", calories: 135 },
+  { name: "coconut", color: "white", calories: 65 },
+  { name: "Dragon Fruit", color: "pink", calories: 105 },
+  { name: "egg plant", color: "purple", calories: 75 },
+];
+
+
+fruits.forEach((fruit)=>{
+
+    console.log(fruit);
+})
