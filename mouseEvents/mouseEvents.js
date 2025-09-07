c1283dc3ff7f21f7551312c593ecaaa13fbb526b
loopystack19 <sanjaygreg19@gmail.com>
2025-09-07 14:58:39 +0300

    i have just learned about addEventListener in js and some of the mouse events out there

---
+++ mouseEvents/mouseEvents.js
@@ -0,0 +1,27 @@
+const myBox = document.querySelector(".myBox");
+
+const clickMeBtn = document.getElementById("action");
+
+function mouseOverEvent() {
+  myBox.style.backgroundColor = "yellow";
+
+  myBox.textContent = "Do it 🥳";
+}
+
+clickMeBtn.addEventListener("mouseover", mouseOverEvent);
+
+function mouseOutEvent() {
+  myBox.style.backgroundColor = "lightgreen";
+
+  myBox.textContent = "click me✌️";
+}
+
+clickMeBtn.addEventListener("mouseout",mouseOutEvent)
+
+function clickEvent() {
+  myBox.style.backgroundColor = "tomato";
+
+  myBox.textContent = "😒 fuck off";
+}
+
+clickMeBtn.addEventListener("click",clickEvent);
---
+++ mouseEvents/mouseEvents.md
@@ -0,0 +1,6 @@
+//eventListener->This listens to a specific event and responds to that event, this makes the page interactive
+
+.addEventListener(event, callback);
+
+**sample programme**
+
