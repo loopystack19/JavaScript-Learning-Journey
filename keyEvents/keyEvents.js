7b9045c7a78274eae07b54337f0ba3193c67f87c
loopystack19 <sanjaygreg19@gmail.com>
2025-09-07 15:34:21 +0300

    a simple key event programme

---
+++ keyEvents/keyEvents.js
@@ -0,0 +1,32 @@
+let myBox = document.getElementById("myBox");
+
+let x = 0;
+
+let y = 0;
+
+let moveAmount = 10;
+
+document.addEventListener("keydown", (event) => {
+  event.preventDefault();
+
+  if (event.key.startsWith("Arrow")) {
+    switch (event.key) {
+      case "ArrowUp":
+        y -= moveAmount;
+        break;
+      case "ArrowDon":
+        y += moveAmount;
+        break;
+      case "ArrowRight":
+        x += moveAmount;
+        break;
+      case "ArrowLeft":
+        x -= moveAmount;
+        break;
+    }
+
+    myBox.style.top = `${y}px`;
+
+    myBox.style.left = `${x}px`;
+  }
+});
