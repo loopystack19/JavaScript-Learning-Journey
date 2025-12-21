842d63706be49ed109fdcb6669bf85c6665f1b05
loopystack19 <sanjaygreg19@gmail.com>
2025-12-21 15:20:54 +0300

    really struggled with object oriented programming and DOM manipulation so am revisting a few stuff

--- objects/objects.js
+++ objects/objects.js
@@ -1,16 +1,36 @@
+//Objects->A collection of related properties or methods that can represent a real world object
+
+//object={key:"value"}
+
 let person1={
-
-    firstName:"patrick",
-    lastName:"star",
-    age:25,
-    isEmployed:false,
-    sayHello:function(){
-
-        console.log("Hello am patrick");
+    firstName:"SpongeBob",
+    lastName:"squarePants",
+    age:27,
+    isEmployed:true,
+    sayHello:()=>{
+        console.log("My name is SpongeBob");
+        console.log("This is my say hello method");
     }
 }
 
-for(let key in person1){
-
-    console.log(person1[key]);
+let person2={
+    "firstName":"Patrick",
+    "lastName":"Star",
+    "age":35,
+    "isEmployed":false,
+    "sayHello":()=>{
+        console.log("Hello am patrick");
+        console.log("This is my hello method")
+    }
 }
+
+console.log(person1.firstName);
+
+console.log(person2["firstName"]);
+
+person2.sayHello();
+
+//Based on these we understand that you can access the properties of different objects based on how they are declared
+
+//The differences of using the brackets and the dot notation comes down to functionality, the bracket notation is way more dynamic
+
