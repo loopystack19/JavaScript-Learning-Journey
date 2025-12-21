19efa1805f253e58bb6c4ff8a9280343f9d05a51
loopystack19 <sanjaygreg19@gmail.com>
2025-12-21 15:31:17 +0300

    This keyword in javascript allows an object's method to reference an object's property

--- This/this.js
+++ This/this.js
@@ -1,1 +1,17 @@
-c
+//This keyword is a reference to the object where the object is used
+
+//person.name=this.name, this depends on the immediate context
+
+//In basic leyman language, this is used inside the object specifically the methods to access the properties
+
+person1={
+    "firstName":"SpongeBob",
+    "favFood":"Burger",
+    "sayHello":function(){
+        console.log(`Hello my name is ${this["firstName"]}`);
+    }
+
+}
+
+person1.sayHello();
+
