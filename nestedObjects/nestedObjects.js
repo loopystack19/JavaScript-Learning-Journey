407db37b7c6800e5cb4c4d865fcfe28a8b6fb5d8
loopystack19 <sanjaygreg19@gmail.com>
2025-12-25 08:09:46 +0300

    those are nested objects fully understood

--- nestedObjects/nestedObjects.js
+++ nestedObjects/nestedObjects.js
@@ -1,33 +1,44 @@
+//Nested Objects in js
+// const person={
+//     fullName:"Spongebob Squarepants",
+//     age:30,
+//     isStudent:true,
+//     hobbies:["Karate","jelly fishing","cooking"],
+//     address:{
+//         street:"124 conch str",
+//         city:"Bikini Bottom",
+//         country:"International waters"
+//     }
+// }
+
 class Address{
 
     constructor(street, city, country){
-
         this.street=street;
-
         this.city=city;
-
         this.country=country;
-
-
     }
 }
 
 class Person{
 
-    constructor(name, age, ...address){
-
+    constructor(name,age,...address){
         this.name=name;
-
         this.age=age;
-
         this.address= new Address(...address);
     }
 }
 
-const person1= new Person("spongeBob",25,"124 conch strt","bikiniBottom", "international waters");
+let person1=new Person("spongebob",30,"124 conch streer",
+                                      "Bikini Bottom",
+                                      "Int waters"
+);
 
+console.log(person1.name);
+console.log(person1.age);
 
-for(const key in person1){
+for(const property in person1.address){
 
-    console.log(person1[key]);
+    console.log(person1.address[property]);
 }
+
