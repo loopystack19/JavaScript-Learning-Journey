f470b62c1c441f95c456a7488b8bdb2445f8fbad
loopystack19 <sanjaygreg19@gmail.com>
2025-12-26 00:06:18 +0300

    that is DOM navigation for you

--- refreshDOM/script.js
+++ refreshDOM/script.js
@@ -55,17 +55,87 @@
 
 //e) document.querySelectorAll()=>This method returns a node list
 
-const foods=document.querySelectorAll("li");
+// const foods=document.querySelectorAll("li");
 
-foods.forEach(food=>{
-    food.style.backgroundColor="blue";
-    food.style.textAlign="center";
-    food.style.padding="20px";
-    food.style.border="1px solid white";
-    food.style.listStyle="none";
-    food.style.color="white";
-    food.style.textTransform="upperCase";
+// foods.forEach(food=>{
+//     food.style.backgroundColor="blue";
+//     food.style.textAlign="center";
+//     food.style.padding="20px";
+//     food.style.border="1px solid white";
+//     food.style.listStyle="none";
+//     food.style.color="white";
+//     food.style.textTransform="upperCase";
+// })
+
+
+//DOM navigation-The process of navigating the structure of a html doc using js
+//.firstElementChild->This selects the first element in an enclosed loop
+
+// const ulElements=document.querySelectorAll("ul");
+
+// ulElements.forEach(ulElement=>{
+
+//     let firstChildElement=ulElement.firstElementChild;
+
+//     firstChildElement.style.background="yellow";
+// });
+
+//.lastElementChild-does the opposite, selects the last element child
+
+
+// ulElements.forEach(ulElement=>{
+
+//     const lastChild=ulElement.lastElementChild;
+
+//     lastChild.style.backgroundColor="red";
+// });
+
+//.nextElementSibling=>This one works with ids, you need to select and element, the nextElementSibling moves by 1 from the selected element
+
+// const appleElement=document.getElementById("apples");
+
+// let nextSibling=appleElement.nextElementSibling;
+
+// nextSibling.style.backgroundColor="lightgreen";
+
+//.previousElementSibling=>does the opposite of nextElementSibling
+
+
+// const coconutElement=document.getElementById("coconut")
+
+// let previousSibling=coconutElement.previousElementSibling;
+
+// previousSibling.style.backgroundColor="blue";
+
+//.parentElement=>This selects the entire thing
+
+// const iceCream=document.getElementById("iceCream");
+
+// let parentElement=iceCream.parentElement;
+
+// parentElement.style.backgroundColor="black";
+
+//.children=>This selects all the children of a selected element
+//it returns a html collection 
+const vegetableUl=document.getElementById("vegetables");
+
+let vegetableChildren=vegetableUl.children;
+
+Array.from(vegetableChildren).forEach(vegetableChild=>{
+    Object.assign(vegetableChild.style , {
+        backgroundColor:"blue",
+        textAlign:"center",
+        padding:"30px",
+        color:"white",
+        border:"1px solid black",
+        margin:"5px"
+    })
 })
+
+
+
+
+
 
 
 
