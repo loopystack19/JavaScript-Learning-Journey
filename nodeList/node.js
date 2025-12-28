9b2854c9315d0b82cf67b14c78a07d9ea5007f32
loopystack19 <sanjaygreg19@gmail.com>
2025-12-28 12:12:55 +0300

    node list

--- nodeList/node.js
+++ nodeList/node.js
@@ -1,19 +1,56 @@
-let myButtons=document.querySelectorAll(".myButtons");
+// node a list a collection like arrays but they don't have in built methods like map, filter and reduce
+
+let myButtons=document.querySelectorAll("button");
 
 myButtons.forEach(button=>{
-    button.style.display="block";
-    button.style.margin="10px";
-    button.style.backgroundColor="blue";
-    button.style.width="150px";
-    button.style.height="25px"
-    button.style.border="none";
-    button.style.color="white";
-    button.style.cursor="pointer";
-})
+    Object.assign(button.style,{
+        backgroundColor:"lightblue",
+        display:"block",
+        width:"100%",
+        padding:"30px",
+        margin:"5px auto",
+        border:"1px solid black"
+    })
+});
+
+//Adding event listener
 
 myButtons.forEach(button=>{
+    button.addEventListener("click",event=>{
+        Object.assign(button.style,{
+            backgroundColor:"blue",
+            color:"white",
+            fontSize:"3rem"
+        })
+    })
+});
 
+//Adding an element to a node list
+
+const newButton=document.createElement("button");// step 1
+
+newButton.textContent="Hello am a new Button 😊";
+// step 2 adding style to the element
+Object.assign(newButton.style,{
+    backgroundColor:"green",
+    color:"white",
+    padding:"30px",
+    width:"100%",
+    height:"125px"
+});
+
+//step 3 Appending the element
+
+document.body.appendChild(newButton);
+
+//After creating this element, it wont automatically reflect in the nodelist because they are static
+
+myButtons=document.querySelectorAll("button");
+
+
+myButtons.forEach(button=>{
     button.addEventListener("click", event=>{
-        event.target.style.backgroundColor="lightgreen";
+        event.target.remove();
+        myButtons=document.querySelectorAll("button");
     })
 })
